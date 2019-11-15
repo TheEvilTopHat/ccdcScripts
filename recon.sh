0ca7979c1e296c8c70ff276ea6f8aa1621e462e6
pressstartearly <57644769+pressstartearly@users.noreply.github.com>
2019-11-15 17:04:03 -0500

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,18 +1,24 @@
 #Getting Server Info
 echo "---Server Information---" > reconReport.txt
 uname -a >> reconReport.txt
-echo ""
+echo "" >> reconReport.txt
 
 #Checking in use ports
-echo "---In use ports---" >> reconReport.txt
+echo "---Ports In Use---" >> reconReport.txt
 netstat -tulpn | grep LISTEN >> reconReport.txt
-echo ""
+echo "" >> reconReport.txt
 
 #Getting all users
 echo "---All Users---" >> reconReport.txt
 less /etc/passwd >> reconReport.txt
-echo ""
+echo "" >> reconReport.txt
+
+#Getting user login log
+echo "---Login Log---" >> reconReport.txt
+last >> reconReport.txt
+echo "" >> reconReport.txt
 
 #Getting all programs currently running
+echo "---Programs Running---" >> reconReport.txt
 ps >> reconReport.txt
-
+echo"" >> reconReport.txt
